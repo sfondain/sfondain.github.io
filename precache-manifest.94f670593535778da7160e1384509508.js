808016c4ea6ebce7e4c3b63dd695c14151f2f58f
sfondain <steeve.fondain@gmail.com>
2019-11-05 18:28:35 +0100

    First commit

---
+++ precache-manifest.94f670593535778da7160e1384509508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cc836ed891672d03a12f",
+    "url": "/js/chunk-vendors.a3b2ae0c.js"
+  },
+  {
+    "revision": "6fa30fe758a078fc658c",
+    "url": "/js/app.30dcf6dc.js"
+  },
+  {
+    "revision": "fe245ef02841cf43cdc43e9ede4700b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc836ed891672d03a12f",
+    "url": "/css/chunk-vendors.33c19d1b.css"
+  },
+  {
+    "revision": "6fa30fe758a078fc658c",
+    "url": "/css/app.d5ce7ee4.css"
+  }
+];
